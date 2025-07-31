47724176885021b1649cf4f99ba66dd77c640640
gregoryb79 <gregoryb79@gmail.com>
2025-07-31 16:22:07 +0300

    all runs localy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,13 @@ import js from "@eslint/js";
 import globals from "globals";
 
 export default [
+  js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
     languageOptions: { globals: globals.browser }
+  },
+  {
+    files: ["**/.eslintrc.js", "**/*.config.js", "src/js/shuffle.js"],
+    languageOptions: { globals: { ...globals.node } }
   }
 ];
