98511e6d6c6a3c7f73af37a5a7133aab0bbd7403
gregoryb79 <gregoryb79@gmail.com>
2025-07-31 16:09:28 +0300

    another fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint";
+import eslint from "eslint";
+const { defineConfig } = eslint;
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
